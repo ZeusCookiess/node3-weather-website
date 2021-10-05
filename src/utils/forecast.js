2d10405e58e6575ccf33fd27db7ebbfa12945d3f
Zeyad Mohamed <zezozm2014@hotmail.com>
2021-10-05 02:10:55 +0200

    forecast.js  changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)        
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' But it feels like ' + body.current.feelslike + '. The humidity is ' + body.current.humidity)      
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' But it feels like ' + 
+            body.current.feelslike + '. The humidity is ' + body.current.humidity)      
         }
     })
     
